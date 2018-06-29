c58d0ed047136d967e17bcf3b755e72a2d4e9bdf
Jack Gonser <jackgonser@gmail.com>
2018-06-29 10:39:47 -1000

    Added c sharp project

---
+++ helloworld/Program.cs
@@ -0,0 +1,41 @@
+﻿// A Hello World! program in C#.
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        public static string searchterm;
+        static void Main()
+        {
+            
+            SearchPropmt();
+            Console.WriteLine(searchterm);
+
+                
+        }
+
+        private static void SearchPropmt()
+        {   string check;
+            bool test = false;
+
+            while (!test) {
+
+
+                Console.WriteLine("Please enter the school name to search.");
+
+                searchterm = Console.ReadLine();
+
+                Console.WriteLine("Just to check, you entered " + searchterm + " to look up. Are you sure? (Y/N)");
+
+                check = Console.ReadLine();
+                if (check == "y") {
+                    test = true;
+                }
+                else {
+                    test = false;
+                }
+            }
+        }
+    }
+}
